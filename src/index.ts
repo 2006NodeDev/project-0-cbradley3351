b68f5790010f5205ed79fc104a60fd685451727b
cbradley3351 <66640819+cbradley3351@users.noreply.github.com>
2020-06-29 02:43:21 -0400

    Practice
    Testing port

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-import express, { Request, Response, NextFunction } from 'express'
+//import express, { Request, Response, NextFunction } from 'express'
 
+
+
+
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+
+    res.setHeader("Content-type", "application/json");
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.writeHead(200); //OK!
+
+    let dataObj = {"Greeting":"Hello"};
+    let data = JSON.stringify(dataObj);
+    res.end(data);
+
+});
+server.listen(1111, function(){
+    console.log('listening on port 1111'); })
